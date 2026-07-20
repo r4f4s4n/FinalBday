e2336b09903d455db897348bdb2b1bda912cd54d
r4f4s4n <45384465+r4f4s4n@users.noreply.github.com>
2026-07-20 16:24:40 +0200

    Revert "Codespace probable doodle 9jq4qxxqvj4fv9x"

--- components/loader.js
+++ components/loader.js
@@ -1,55 +1,20 @@
-function getLoaderOptions(options) {
-    const globalOptions = window.__FB_LOADER_OPTIONS || {};
-    return {
-        stripParentPrefix: false,
-        replaceAssetParentPrefix: false,
-        ...globalOptions,
-        ...(options || {})
-    };
-}
-
-function normalizeFetchPath(path, options) {
-    if (options.stripParentPrefix && path.startsWith('../')) {
-        return path.slice(3);
-    }
-    return path;
-}
-
-function normalizeComponentMarkup(markup, options) {
-    if (!options.replaceAssetParentPrefix) {
-        return markup;
-    }
-    return markup.replace(/\.\.\/assets\//g, 'assets/');
-}
-
-async function loadComponentInto(selector, url, options) {
+async function loadComponentInto(selector, url) {
     const container = document.querySelector(selector);
     if (!container) {
         return;
     }
 
-    const effectiveOptions = getLoaderOptions(options);
-    const normalizedUrl = normalizeFetchPath(url, effectiveOptions);
-    const cachedMarkup = window.FinalBdayAssetCache && typeof window.FinalBdayAssetCache.getText === 'function'
-        ? window.FinalBdayAssetCache.getText(normalizedUrl)
-        : '';
-
-    let rawMarkup = cachedMarkup;
-    if (!rawMarkup) {
-        const response = await fetch(normalizedUrl, { cache: 'no-store' });
-        if (!response.ok) {
-            throw new Error(`No se pudo cargar ${normalizedUrl}: ${response.status}`);
-        }
-
-        rawMarkup = await response.text();
+    const response = await fetch(url, { cache: 'no-store' });
+    if (!response.ok) {
+        throw new Error(`No se pudo cargar ${url}: ${response.status}`);
     }
-    container.innerHTML = normalizeComponentMarkup(rawMarkup, effectiveOptions);
+
+    container.innerHTML = await response.text();
 }
 
-async function loadComponents(components, options) {
+async function loadComponents(components) {
     const entries = Object.entries(components);
-    await Promise.all(entries.map(([selector, url]) => loadComponentInto(selector, url, options)));
+    await Promise.all(entries.map(([selector, url]) => loadComponentInto(selector, url)));
 }
 
-window.loadComponentInto = loadComponentInto;
 window.loadComponents = loadComponents;
